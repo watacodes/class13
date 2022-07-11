44b9242f2c79713ccaa80e5810f564a33de4cf7d
Wataru Hori <bireleysaddiction@gmail.com>
2022-07-11 22:57:37 +0900

    Practiced dom manipulation and functions

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+const placeholder = document.getElementById('placeholder');
+
+document.getElementById('submit-button').addEventListener('click', convertTemp);
+
+function convertTemp() {
+    const tempInCelcius = document.getElementById('celcius').value;
+    if (tempInCelcius === '') {
+        return alert('Undefined');
+    }
+    let convertedTemp = (tempInCelcius * 9 / 5) + 32;
+    return placeholder.innerText = `${tempInCelcius} degree(s) in Celcius is ${convertedTemp} degrees in Fahrenheit!`;
+};
+
