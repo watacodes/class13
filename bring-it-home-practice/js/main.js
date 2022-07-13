5295a4fa63ad5a04507d558217f1c8b4446d28f0
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-13 17:55:07 +0900

    Practiced functions and conditionals

--- bring-it-home-practice/js/main.js
+++ bring-it-home-practice/js/main.js
@@ -1,50 +1,52 @@
 // *Variables*
 // Create a variable and console log the value
 
-let newVariable = 'monkeyman';
-console.log(newVariable);
+let newVar = 12;
+
+console.log(newVar);
 
 // Create a variable, add 10 to it, and alert the value
 
-let secondVariable = 12;
-secondVariable += 10;
+let newNum = 25;
+newNum += 10;
 
-alert(secondVariable);
+alert(`The value of the variable newNum is ${newNum}`);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
 
-function subtractFourNum(a, b, c, d) {
+function subtractFour(a, b, c, d) {
     let result = a - b - c - d;
     alert(result);
 };
 
-subtractFourNum(55, 2, 1, 2);
+subtractFour(55, 2, 1, 2);
 
 // Create a function that divides one number by another and returns the remainder
 
-const divideTwo = (a, b) => {
+function divideTwo(a, b) {
     let remainder = a % b;
-    alert(remainder); // return remainder; would be the answer
+    alert(remainder); // return remainder;
 };
 
-divideTwo(5, 2);
+divideTwo(6, 4);
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
-function addTwoNumbers(a, b) {
-    a + b > 50 ? alert('Jumanji') : alert('The sum of two numbers is less than 50!');
-}
+function isSumGreaterThan50(a, b) {
+    let testNum = a + b;
+    testNum > 50 ? alert('Jumanji') : alert('The sum is less than 50.');
+};
 
-addTwoNumbers(42, 12);
+isSumGreaterThan50(51, 2);
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
 
-function multiplyThreeNumbers(a, b, c) {
-    let multiply = a * b * c;
-    (multiply % 3 == 0 ) ? alert('ZEBRA') : alert('The number is not divisible by 3!');
-} 
+function multiplyThree(a, b, c) {
+    let multipliedNum = a * b * c;
+    multipliedNum % 3 == 0 ? alert('ZEBRA') : alert('The multiplied number is not divisible by 3.');
+};
 
-multiplyThreeNumbers(3, 27, 9);
+multiplyThree(3, 9 , 27);
 
