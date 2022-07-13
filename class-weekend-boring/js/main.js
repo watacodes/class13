ad683fda622a2ecc5c68c53b97587a841d073e0a
Wataru Hori <Bireleysaddiction@gmail.com>
2022-07-13 18:08:19 +0900

    Practiced DOM manipulation and functions

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -1,22 +1,18 @@
-// Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
-// Tuesday and Thursday = class day, Saturday and Sunday = weekend
-
+document.getElementById('check').addEventListener('click', checkDays);
 const placeholder = document.getElementById('placeToSee');
-const checkButton = document.getElementById('check');
-let daysOfTheWeek = document.getElementById('day');
-
-checkButton.addEventListener('click', checkDays);
 
 function checkDays() {
-  let day = daysOfTheWeek.value.toLowerCase();
-  console.log(day);
-  if (day === "tuesday" || day === "thursday") {
-    placeholder.innerText = 'It\'s a class day!';
-  } else if (day === "saturday" || day === "sunday") {
-    placeholder.innerText = 'It\'s weekend, Yay!';
+  let userDay = document.getElementById('day').value.toLowerCase();
+  const daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  if (!daysOfTheWeek.includes(userDay)) {
+    alert('Please enter a valid day of the week!');
+  } else if (userDay === daysOfTheWeek[1] || userDay === daysOfTheWeek[3]) {
+    placeholder.innerText = `It's ${userDay}, class time!`;
+  } else if (userDay === daysOfTheWeek[5] || userDay === daysOfTheWeek[6]) {
+    placeholder.innerText = `${userDay} is weekend, Yay!`;
   } else {
-    placeholder.innerText = 'It\'s just a day, boring!';
+    let capitalize = userDay.charAt(0).toUpperCase() + userDay.slice(1);
+    placeholder.innerText = `${capitalize} is a boring day! ;(`;
   }
-}
-
+};
 
